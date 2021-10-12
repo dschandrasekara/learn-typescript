427db63acdc28d86ffb6c1491cf1457845d5282f
Dinuka Dayarathna <dinuka@huubap.com>
2021-10-12 20:13:31 +0530

    bubble sort execute commands

--- Sorts/bubble-sort.ts
+++ Sorts/bubble-sort.ts
@@ -1,3 +1,6 @@
+//compile command -  FILENAME=bubble-sort FOLDER=Sorts npm run compile
+//output command -  FILENAME=bubble-sort FOLDER=Sorts npm run output
+
 // Bubble sort is a simple sorting algorithm. 
 // This sorting algorithm is comparison-based algorithm in which each pair of adjacent elements is compared and the elements are swapped if they are not in order.
 // This algorithm is not suitable for large data sets as its average and worst case complexity are of Ο(n2).
