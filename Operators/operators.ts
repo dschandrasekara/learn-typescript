acf052b6bbfabc451fd5188f726e30dd84e10760
Lakmal <lakmal@huubap.com>
2021-10-13 12:24:36 +0530

    Added TypeScript - Operator example

---
+++ Operators/operators.ts
@@ -0,0 +1,15 @@
+var x = 4;
+var y = -x;
+console.log("value of x: ", x);
+console.log("value of y: ", y);
+/*An operator defines some function that will be performed on the data.
+The data on which operators work are called operands.
+Consider the following expression −
+7 + 5 = 12
+Here, the values 7, 5, and 12 are operands, while + and = are operators.
+
+The major operators in TypeScript can be classified as −
+
+compile command FILENAME=Operator  FOLDER=operators npm run compile
+run command     FILENAME=Operator  FOLDER=operators npm run output
+*/
