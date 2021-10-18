cf969fc37ea85c3bb211e969f8c9d1e1f665bae3
Aravind Harinarayanan <57535834+AravindHari@users.noreply.github.com>
2021-10-18 19:31:37 +0530

    Update Switch.ts

--- Switch/Switch.ts
+++ Switch/Switch.ts
@@ -1,3 +1,9 @@
+/** Steps to run the file
+1) First install Typescript: `npm install -g typescript`
+2) Create file
+3) In command prompt run `tsc helloworld.ts`
+4) Run the command `node helloworld.js`*/
+
 let day : number = 3
 
 switch (day) {
