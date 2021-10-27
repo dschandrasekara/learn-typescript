5d2c43b0a5b734c9b25754b49b3f4e9b01782df1
Jani Paasiluoto <jani.paasiluoto@innokasmedical.fi>
2021-10-27 12:28:42 +0300

    added comments

--- Functions/async-function.ts
+++ Functions/async-function.ts
@@ -1,14 +1,19 @@
+// an async function which first prints 'calling', then it waits for resolveAfter2Seconds() to be resolved and after two seconds, it prints 'resolved'
+
 function resolveAfter2Seconds() {
+    // define a Promise 
     return new Promise(resolve => {
+        // a resolved promise after certain time
       setTimeout(() => {
         resolve('resolved');
-      }, 2000);
+      }, 2000); // resolves after 2 seconds 
     });
   }
   
+  // async/await will always return a Promise, even if you omit the Promise keyword
   async function asyncCall() {
     console.log('calling');
-    const result = await resolveAfter2Seconds();
+    const result = await resolveAfter2Seconds(); // here we wait for the resolveAfter2Seconds() -function to resolve the Promise and print the result
     console.log(result);
     // expected output: "resolved"
   }
